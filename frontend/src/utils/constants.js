b080f8eebe1a574ee8e9f83ab71955e1f79b6fe3
sharom bromberg <sharombg7@gmail.com>
2025-11-08 18:14:23 -0500

    updated constants in frontend to connect with backend

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,4 +1,7 @@
 export const INITIAL_LIMIT = 8;
 export const FREE_SHIPPING_THRESHOLD = 500000;
 export const SHIPPING_FEE = 20000;
-export const BASE_URL = 'http://localhost:4000/';
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ??
+  process.env.VITE_BACKEND_URL ??
+  "https://proyecto-nucba-fullstack-1.onrender.com/";
