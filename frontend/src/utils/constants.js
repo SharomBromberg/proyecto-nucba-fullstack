fedbd17c25c00918d4062cbebf00a35c2e356a42
sharom bromberg <sharombg7@gmail.com>
2025-11-08 22:47:53 -0500

    fixed constants in front

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -2,4 +2,3 @@ export const INITIAL_LIMIT = 8;
 export const FREE_SHIPPING_THRESHOLD = 500000;
 export const SHIPPING_FEE = 20000;
 export const BASE_URL = 'https://proyecto-nucba-fullstack-1.onrender.com/';
-;
