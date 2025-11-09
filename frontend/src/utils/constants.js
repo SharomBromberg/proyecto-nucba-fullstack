8b856baf68f09397474aa2484bc0b60a9137ee62
sharom bromberg <sharombg7@gmail.com>
2025-11-08 23:03:49 -0500

    fixed constants issue

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -4,4 +4,4 @@ export const SHIPPING_FEE = 20000;
 const envBaseUrl = import.meta.env.VITE_BACKEND_URL;
 
 export const BASE_URL =
-  envBaseUrl ?? "https://proyecto-nucba-fullstack-1.onrender.com";
+  envBaseUrl ?? "https://proyecto-nucba-fullstack-1.onrender.com" ;
